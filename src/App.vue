01a4e76dd671fa91ed61aa030fb9ba8cdafc012b
mint_duc <mintduc2612@gmail.com>
2025-03-17 00:05:26 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import Todo from './components/todo/Todo.vue';
+
+const valueStr = ref("");
+let editMode = ref(false);
+const listTodo = ref([
+  {
+    id: 1,
+    content: "Làm việc nhà"
+  }
+])
+
+function handleAddTodo(){
+  if(!valueStr.value.length){
+    return
+  }
+  listTodo.value.push({
+    id: listTodo.value.length + 1,
+    content: valueStr.value
+  })
+  valueStr.value = ""
+}
+
+function handleClickTodo(){
+  console.log('heloo coin')
+}
+</script>
+
+<template>
+  <h1 class="font-bold text-2xl mb-5">Todo App</h1>
+  <div class="flex mb-10">
+      <input type="text" id="first_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-1/6 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+       required 
+      placeholder="Nhập việc muốn làm" v-model="valueStr"
+      />
+      <button type="button" @click="handleAddTodo" class="ml-3 cursor-pointer text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+        {{ editMode ? 'Edit' : 'Add' }}
+      </button>
+  </div>
+  <li v-for="item in listTodo">
+    <Todo :content="item.content" @click="handleClickTodo"/>
+  </li>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/todo/Todo.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+defineProps<{ content: string,onClick: () => void }>()
+</script>
+
+<template>
+    <span class="text-2xl cursor-pointer" @click="onClick">{{ content }}</span>
+    <button type="button" class="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:focus:ring-yellow-900">Edit</button>
+    <button type="button" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Delete</button>
+</template>
+
+<style scoped>
+span{
+    margin-right: 10px;
+}
+</style>
